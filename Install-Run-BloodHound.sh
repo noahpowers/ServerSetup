2bc20476af7ec2d769cbcb2980b9ad0c79a8c62e
owz <npowers@delta-risk.net>
2018-11-06 23:45:37 +0000

    Fixed BloodHound download link for v2.0.4

--- Install-Run-BloodHound.sh
+++ Install-Run-BloodHound.sh
@@ -11,7 +11,7 @@ apt-get install neo4j -y
 
 ### Download new Bloodhound
 ### https://github.com/BloodHoundAD/BloodHound/releases
-wget -O BloodHound-linux-x64.zip https://github.com/BloodHoundAD/BloodHound/releases/download/2.0.3.1/BloodHound-linux-x64.zip
+wget -O BloodHound-linux-x64.zip https://github.com/BloodHoundAD/BloodHound/releases/download/2.0.4/BloodHound-linux-x64.zip
 unzip BloodHound-linux-x64.zip
 cp -a BloodHound-linux-x64/ /opt/
 
